793fd102919614ca7f0b10ce747275b4815b2d66
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-17 15:13:05 +0000

    Push tables and colums to database

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,39 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_816_145_504) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_145504) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cars', force: :cascade do |t|
-    t.string 'car_model'
-    t.text 'description'
-    t.string 'photo'
-    t.decimal 'reservation_price'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_cars_on_user_id'
+  create_table "cars", force: :cascade do |t|
+    t.string "car_model"
+    t.text "description"
+    t.string "photo"
+    t.decimal "reservation_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
-  create_table 'reservations', force: :cascade do |t|
-    t.string 'city'
-    t.date 'date'
-    t.bigint 'user_id', null: false
-    t.bigint 'car_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['car_id'], name: 'index_reservations_on_car_id'
-    t.index ['user_id'], name: 'index_reservations_on_user_id'
+  create_table "reservations", force: :cascade do |t|
+    t.string "city"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'cars', 'users'
-  add_foreign_key 'reservations', 'cars'
-  add_foreign_key 'reservations', 'users'
+  add_foreign_key "cars", "users"
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
