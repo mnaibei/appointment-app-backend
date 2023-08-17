002cc4e74ff58a5ccb621d903c93c16b63ef8784
mnaibei <muchajulius@gmail.com>
2023-08-17 15:51:09 +0300

    Modified routes.rb to add a simpler path to get user reservations

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :cars, only: %i[index show create destroy] do
       resources :reservations, only: %i[index show create destroy]
     end
+    resources :reservations, only: %i[index show create destroy]
   end
 end
