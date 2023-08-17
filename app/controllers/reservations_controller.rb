53c0ab962e114a1d4fb9c4bba936e285a2176f4c
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-17 11:14:17 +0000

    Implement CRUD actions

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,47 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    render json: @reservation
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Reservation not found' }, status: :not_found
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    if @reservation.update(reservation_params)
+      render json: @reservation
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Reservation not found' }, status: :not_found
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:city, :user_id, :car_id)
+  end
 end
