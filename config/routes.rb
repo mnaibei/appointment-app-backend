928d706eeeffa965c5fd5177aa03618830f62f72
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-17 12:12:22 +0000

    Make routes readable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :users, only: %i[index create] do
-    resources :cars, only: %i[index show create destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index create] do
+        resources :cars, only: %i[index show create destroy] do
+          resources :reservations, only: %i[index show create destroy]
+        end
+      end
+    end
   end
 end
