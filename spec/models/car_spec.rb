0e0b92f10473713c036bbfe19a027803d00d401f
mnaibei <muchajulius@gmail.com>
2023-08-17 22:41:50 +0300

    Added more tests to car spec

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -35,4 +35,37 @@ RSpec.describe Car, type: :model do
         )
         expect(car).not_to be_valid
       end
+
+      it "is not valid without a photo" do
+        car = Car.new(
+          car_model: 'Sedan',
+          description: 'A comfortable sedan for daily use.',
+          photo: nil,
+          reservation_price: 100.00,
+          user: user
+        )
+        expect(car).not_to be_valid
+      end
+    
+      it "is not valid without a reservation_price" do
+        car = Car.new(
+          car_model: 'Sedan',
+          description: 'A comfortable sedan for daily use.',
+          photo: 'sedan.jpg',
+          reservation_price: nil,
+          user: user
+        )
+        expect(car).not_to be_valid
+      end
+    
+      it "is not valid if reservation_price is not greater than 0" do
+        car = Car.new(
+          car_model: 'Sedan',
+          description: 'A comfortable sedan for daily use.',
+          photo: 'sedan.jpg',
+          reservation_price: 0,
+          user: user
+        )
+        expect(car).not_to be_valid
+      end
 end
