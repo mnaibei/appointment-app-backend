3f6198d066aa9d5bfeb11454a0fb9ea3906ba322
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-17 23:58:15 +0000

    Add devise data to database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :users, only: %i[index create] do
     resources :cars, only: %i[index show create destroy] do
       resources :reservations, only: %i[index show create destroy]
