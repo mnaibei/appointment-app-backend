594460905a8953d90f4c617a6ad2aea897c94019
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-20 03:48:37 +0000

    setup routes in other to handel registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'private/test'
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
   resources :users, only: %i[index create] do
     resources :cars, only: %i[index show create destroy] do
       resources :reservations, only: %i[index show create destroy]
