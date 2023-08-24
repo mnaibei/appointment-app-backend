a0af70508eeb5675e347371c9f560b2ddbfea699
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-24 02:12:19 +0000

    Make changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :role])
   end
 end
