bf0f9731a4e639801400a07e114652dff5c433d4
Obed Bamfo <obedbamfo5@gmail.com>
2023-08-24 02:14:43 +0000

    Reposition columns

--- db/migrate/20230819114735_add_devise_to_users.rb
+++ db/migrate/20230819114735_add_devise_to_users.rb
@@ -2,6 +2,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
+      t.integer :role, default: 0
       t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
